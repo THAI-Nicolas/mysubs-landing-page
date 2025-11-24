4bd24b74c05deb0c01698c291ee2158df142cd68
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2025-11-24 23:08:40 +0100

    Publication de la V1 sur Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://mysubs.app',
+  site: "https://mysubs.app",
+  output: "static",
+  adapter: netlify(),
 });
